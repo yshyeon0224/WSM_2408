c953d7923f0252b6a618feab6bdb32a1a2236eb3
yshyun0224 <w2122@e-mirim.hs.kr>
2022-09-22 15:10:00 +0900

    이전 다음 월 달력 완성

--- 20220901_grid/js/dynamic.js
+++ 20220901_grid/js/dynamic.js
@@ -20,6 +20,7 @@ const setcalendar = (year, month) => {
 
     //1~lastDate까지 반복
     const dateGridContainerDiv = document.getElementsByClassName("date-grid-container")[0];
+    dateGridContainerDiv.innerHTML = ''; //초기화
 
     for (let i = 1; i<=lastDate; i++) {
         //<div class ="grid-item">$</div>
@@ -38,6 +39,36 @@ const setcalendar = (year, month) => {
 
     ///1일: grid-column-start: 요일+1
 }
+
+
+//prevMonth 함수
+const prevMonth = () => {
+    //이전 월 구하기
+    month--;
+    //setcalendar 함수 호출
+    if (month == 0){
+        month = 12;
+        year --;
+    }
+    setcalendar(year, month);
+}
+
+//nextMonth 함수
+const nextMonth = () => {
+    //다음 월 구하기
+    month ++;
+    //setcalendar 함수 호출
+    if (month == 13){
+        month = 1;
+        year ++;
+    }
+    setcalendar(year, month);
+}
+
+//prev 버튼 누르면 prevMonth 함수 실행
+prev_btn.onclick = prevMonth; 
+//next 버튼 누르면 nextMonth 함수 실행
+next_btn.onclick = nextMonth;
 
 //오늘 날짜
 let today = new Date();
