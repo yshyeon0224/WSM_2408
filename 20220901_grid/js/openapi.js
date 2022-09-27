f5d18ca54fe6e5560f00e145f6d9ba0df1a313fc
yshyun0224 <w2122@e-mirim.hs.kr>
2022-09-27 14:05:15 +0900

    start 급식 openAPI

---
+++ 20220901_grid/js/openapi.js
@@ -0,0 +1,18 @@
+//시도 교육청 코드 서울 B10
+// 표준 학교 코드 : 7010569
+
+//https://open.neis.go.kr/hub/mealServiceDietInfo
+//KEY: e7f5eb3d7bc04c0898a56121aac82cb9
+//ATPT_OFCDC_SC_CODE : 시도교육청코드
+//SD_SCHUL_CODE 학교 코드
+//MMEAL_SC_CODE : 식사코드
+//MLSV_YMD : 급식일자
+//MLSV_FROM_YMD : 급식 시작 일자
+//MLSV_TO_YMD : 급식 종료 일자
+//https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010569&MMEAL_SC_CODE=2&MLSV_YMD=20220927&Type=json
+
+const KEY = "e7f5eb3d7bc04c0898a56121aac82cb9";
+const ATPT_OFCDC_SC_CODE = "B10";
+const SD_SCHUL_CODE = "7010569"
+let MMEAL_SC_CODE = 2; //중식
+let MLSV_YMD = "20220927"; //YYYYMMDD
